158532245007a24ac58a1ebcbe6179bd982ba619
Ubuntu <ubuntu@ip-172-31-0-151.us-west-2.compute.internal>
2013-07-29 03:13:02 +0000

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Unicorn::Application.config.secret_token = '0edc1cc39d0280a953087b19f0f588721b84839cbbb873cf75d52ac23d2b2666c9132b401753bc467f723f5991505041d8f998b6b8632e36cae0c647587cbefb'
